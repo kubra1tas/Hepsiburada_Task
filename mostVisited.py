9dffa4aa9a0f34f899338e72f4a6a9177ddab6d6
Kubra <kubratas08@gmail.com>
2021-07-12 14:10:00 +0300

    Update comments

--- mostVisited.py
+++ mostVisited.py
@@ -1,8 +1,15 @@
+#!/usr/bin/python3.6
+# Kubra Tas July 2021
+
+
 import pandas as pd
 import json
-with open('data/data/events.json', 'r', encoding='utf-8') as f:
+import os
+
+
+with open(os.getcwd() + '/data/data/events.json', 'r', encoding='utf-8') as f:
     prd = json.load(f)
 df = pd.io.json.json_normalize(prd["events"])
 
-df2 = df.groupby("productid").count().sort_values(by="event",ascending=False)[:10]
-df2.to_csv("top10mostVisited.csv")
+df2 = df.groupby("productid").count().sort_values(by="event",ascending=False)[:10] #group by the data, sort values and return 10 most added
+df2.to_csv(os.getcwd() + "top10mostVisited.csv")
